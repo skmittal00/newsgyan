8bfd4abde8e8aba8131a93deffe124ac38bd1d69
gitmat <gitgat21@gmail.com>
2022-03-01 20:50:29 +0530

    this is first commit for Newsygan

---
+++ Server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const connection=require('./routes/Connection.js');
+//const BodyParser = require("body-parser");
+var cors=require("cors");
+
+const mongoose=require("mongoose");
+//var CtrlUser=require("./Controller/controller.users");
+var routeapp=require("./App/route.js");
+
+//connection();
+
+const port = process.env.PORT || 3000;
+//app.use("/api",CtrlUser.allusers);
+
+app.use("/api",routeapp);
+app.listen(port, () => console.log("You are at 3000 port" + port)); 
